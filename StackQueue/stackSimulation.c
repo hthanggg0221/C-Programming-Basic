af4c52b418b6d49e94e587b3d3146fdc0d24a02c
Hoàng Văn Thắng <143022198+hthanggg0221@users.noreply.github.com>
2024-12-10 17:14:44 +0700

    Updated

--- StackQueue/stackSimulation.c
+++ StackQueue/stackSimulation.c
@@ -4,80 +4,77 @@
 
 typedef struct Node
 {
-    int value;
-    struct Node* next;
+	int value;
+	struct Node* next;
 } Node;
 
-Node* top;
-
-Node* makeNode(int x)
-{
-    Node *p = (Node*)malloc(sizeof(Node));
-    p->value = x; p->next = NULL;
-    return p;
-}
+Node *top;
 
 void initStack()
 {
-    top = NULL;
+	top = NULL;
 }
 
-int stackEmpty()
+int emptyStack()
 {
-    return top == NULL;
+	return top == NULL;
+}
+
+Node *makeNode(int val)
+{
+	Node *p = (Node*)malloc(sizeof(Node));
+	p->value = val;
+	p->next = NULL;
+	return p;
+}
+
+void push(int val)
+{
+	Node *p = makeNode(val);
+	p->next = top;
+	top = p;
 }
 
 int pop()
 {
-    int x = top->value;
-    Node* p = top;
-    top = top->next;
-    free(p);
-    return x;
+	int x = top->value;
+	Node *tmp = top;
+	top = top->next;
+	free(tmp);
+	return x;
 }
 
-void push(int x)
+int freeAll()
 {
-    Node *p = makeNode(x);
-    p->next = top;
-    top = p;
+	while (!emptyStack())
+	{
+		Node *tmp = top;
+		top = top->next;
+		free(tmp);
+	}
 }
 
-void freeAll()
-{
-    while(!stackEmpty())
-    {
-        Node *p = top;
-        top = top->next;
-        free(p);
-    }
-}
-
-int main()
-{
-    char cmd[10];
-    while(1)
-    {
-        scanf("%s", cmd);
-        if(strcmp(cmd, "#") == 0)
-            break;
-        else if (strcmp(cmd, "PUSH") == 0)
-        {
-            int v;
-            scanf("%d", &v);
-            push(v);
-        }
-        else if (strcmp(cmd, "POP") == 0)
-        {
-            if(stackEmpty())
-                printf("NULL\n");
-            else
-            {
-                int v = pop();
-                printf("%d\n", v);
-            }
-        }
-    }
-    freeAll();
+int main(){
+	char cmd[5];
+	while (1)
+	{
+		scanf("%s", cmd);
+		if(strcmp(cmd,"#") == 0)
+			break;
+		else if(strcmp(cmd, "PUSH") == 0)
+		{
+			int v;
+			scanf("%d", &v);
+			push(v);
+		}
+		else if(strcmp(cmd, "POP") == 0)
+		{
+			if(emptyStack())
+				printf("NULL\n");
+			else
+				printf("%d\n", pop());
+		}
+	}
+	freeAll();
     return 0;
 }
